62cb0604a2f477a2edc4db685cd8544842e83aa0
shefat2002 <shefat2002@gmail.com>
2025-02-15 07:38:32 +0600

    Added a simple CRUD MVC web application

---
+++ SimpleCRUDApp/SimpleCRUDApp/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleCRUDApp.Data;
+using SimpleCRUDApp.Models;
+
+namespace SimpleCRUDApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _context.Products.ToList();
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ SimpleCRUDApp/SimpleCRUDApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimpleCRUDApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
