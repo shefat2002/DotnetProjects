d3f75aa596d84b21ce0d892c83252c3f210129ca
shefat2002 <shefat2002@gmail.com>
2025-02-15 07:39:15 +0600

    Added a Expense Tracker MVC web application

---
+++ ExpenseTracker/ExpenseTracker/Program.cs
@@ -0,0 +1,43 @@
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            //Dependency Injection
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+            var app = builder.Build();
+
+            //Register Syncfusion license
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2UlhhQlVMfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTX9SdERjWnxacHRWQGRc");
+
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
