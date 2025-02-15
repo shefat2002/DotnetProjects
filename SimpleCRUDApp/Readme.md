bd3467d435b41dcfbd9928c08b2f2aac21bda897
shefat2002 <shefat2002@gmail.com>
2025-02-15 12:25:26 +0600

    Updated a few files

--- SimpleCRUDApp/Readme.md
+++ SimpleCRUDApp/Readme.md
@@ -5,3 +5,9 @@ In this project you can add, edit, delete and view list of products.</br></br>
 <b>Frontend:</b> HTML, CSS, Bootstrap</br>
 <b>Backend:</b> C#, ASP.NET Core 9.0</br>
 <b>Database:</b> SQL Server</br>
+</br></br>
+<h3>Screenshots:</h3>
+<a href="\Screeenshots\Product-list.png", alt="Product List"></a>
+<a href="\Screeenshots\Create.png", alt="Create Product"></a>
+<a href="\Screeenshots\Successful.png", alt="Successfull Notification"></a>
+<a href="\Screeenshots\Delete-warning.png", alt="Delete Warning"></a>
--- SimpleCRUDApp/SimpleCRUDApp/Controllers/ProductController.cs
+++ SimpleCRUDApp/SimpleCRUDApp/Controllers/ProductController.cs
@@ -22,5 +22,63 @@ namespace SimpleCRUDApp.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create([Bind("Name, Price, Description, Stock")] Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(model);
+                _context.SaveChanges();
+                TempData["Notification"] = "Product Created Successfully!";
+                TempData["NotificationType"] = "success";
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = _context.Products.Find(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Edit([Bind("Id, Name, Price, Description, Stock")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Update(product);
+                _context.SaveChanges();
+                TempData["Notification"] = "Product Updated Successfully!";
+                TempData["NotificationType"] = "success";
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = _context.Products.Find(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        public IActionResult DeleteConfirm(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+                TempData["Notification"] = "Product Deleted Successfully!";
+                TempData["NotificationType"] = "success";
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
